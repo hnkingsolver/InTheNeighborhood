598832a15de92c5121b450b14efaa1a0213f5144
Hannah Kingsolver <58765774+hnkingsolver@users.noreply.github.com>
2020-07-24 17:32:58 -0500

    nav added

--- inTheNeighborhood/itn/views.py
+++ inTheNeighborhood/itn/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render, HttpResponse, redirect
 
 # Create your views here.
 def index(request):
-    return HttpResponse("this is the equivalent of @app.route('/')!")
+    return render(request, "about.html")
