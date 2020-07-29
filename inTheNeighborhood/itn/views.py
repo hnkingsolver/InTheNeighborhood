12beeec5979c7560eb847f54ac18c098c1e284ef
Hannah Kingsolver <58765774+hnkingsolver@users.noreply.github.com>
2020-07-29 16:06:18 -0500

    Update views.py

--- inTheNeighborhood/itn/views.py
+++ inTheNeighborhood/itn/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render, HttpResponse, redirect, render
 
-# Create your views here.
-from django.shortcuts import render	# notice the import!
+
+
 def index(request):
-<<<<<<< HEAD
     return render(request, "index.html")
-=======
+
+def about(request):
     return render(request, "about.html")
->>>>>>> hannah
