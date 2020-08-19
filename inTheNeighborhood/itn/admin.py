620332d0c95331303fa749682bef8834a3dd8011
Hannah Kingsolver <58765774+hnkingsolver@users.noreply.github.com>
2020-08-18 21:05:36 -0500

    updates to beauty brands and products page

--- inTheNeighborhood/itn/admin.py
+++ inTheNeighborhood/itn/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Artist, Restaurant, BeautyBrand, Book
+from .models import Artist, Restaurant, BeautyBrand, Book, Article
 
 admin.site.register(Artist)
 admin.site.register(Restaurant)
 admin.site.register(BeautyBrand)
 admin.site.register(Book)
+admin.site.register(Article)
+
