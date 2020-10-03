197a153411e0de2d3c6848d190d17855a534fabe
Hannah Kingsolver <58765774+hnkingsolver@users.noreply.github.com>
2020-10-02 22:57:53 -0500

    Revert "deployment"
    This reverts commit 8667a58ff3623497056565ce176fba4d27fddcec.

--- inTheNeighborhood/inTheNeighborhood/settings.py
+++ inTheNeighborhood/inTheNeighborhood/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '-j^)lhibj+hsl=#z(g*=+h%3exk)c6=elb91r_k!o=zr(q$td('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['neighborhood-developers.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
